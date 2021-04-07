ff03373d4cab7176b27a4d19a7b17eb4bc3b300e
yun <>
2021-04-07 14:42:53 +0800

    add all_file

---
+++ all_file.py
@@ -0,0 +1,45 @@
+import os
+
+
+def all_file_of_dir(dir, files):
+    '''
+    目录下所有文件
+    :param dir:
+    :param files:
+    :return:
+    '''
+    get_dir = os.listdir(dir)
+
+    for i in get_dir:
+
+        sub_dir = os.path.join(dir, i)
+
+        if os.path.isdir(sub_dir):
+
+            all_file_of_dir(sub_dir, files)
+
+        else:
+            files.append(os.path.join(dir, i))
+
+
+def combine_all_file(dir, out_file, suffix):
+    '''
+    结合所有文件
+    :param dir:
+    :param out_file:
+    :return:
+    '''
+    fp = open(out_file, "w")
+
+    files = []
+    all_file_of_dir(dir, files)
+
+    for n in files:
+        if suffix is not None:
+            if n.endswith(suffix) is False:
+                continue
+
+        print(n)
+        sfp = open(n)
+        ls = sfp.readlines()
+        fp.writelines(ls)
---
+++ test.py
@@ -0,0 +1,8 @@
+from all_file import combine_all_file
+
+if __name__ == "__main__":
+    dir = "/Volumes/mac_data/2-pj/99-alarm/alarm-server/alarm-capi/src/main/java/com/kd/alarm/capi"
+
+    save_file = "/Users/yun/Downloads/a1.txt"
+
+    combine_all_file(dir, save_file, '.java')
